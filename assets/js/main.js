151ae5c33f6f8188bb99e2fcee2f7f5064a450d8
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-29 10:47:39 -0300

    Agregando botones y checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 	$("#button").click(function(e){
 		e.preventDefault();
-		var tarea= $("#textarea1").val();
+		var tarea= $("#textarea1").val();// trae el valor ingresado por el usuario
 		if(tarea == "" && tarea == ""){
 			alert("Debes escribir una tarea");
-		}else{
-			$("#comentarios").append('<div><p class="hola">'+ tarea +'</p></div>');
+		}else{// si el usuario ingresa una tarea, esta se mostrara  junto al checkbox y el boton remove
+			$("ul").append('<div><form action="#"><p><input type="checkbox" id="test5"/><label label for="test5" id="comentarios">' + tarea +'</label></p></div>' + '<a class="waves-effect waves-light btn btn-mio remover">Remove</a>');
+	
 		}
-			
 	});	
+		$(document).on('click' , '.remover' , function(){
+		$(this).parent().remove();
+ 	});
 })
