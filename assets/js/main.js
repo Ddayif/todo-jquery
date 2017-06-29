9084a2202fe266c296f2e4d1ab4b7a2a3f6333ee
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-29 11:18:02 -0300

    Arreglando jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 	$("#button").click(function(e){
+		 localStorage.setItem('listItems', $("#comentarios").html());
 		e.preventDefault();
 		var tarea= $("#textarea1").val();// trae el valor ingresado por el usuario
 		if(tarea == "" && tarea == ""){
 			alert("Debes escribir una tarea");
 		}else{// si el usuario ingresa una tarea, esta se mostrara  junto al checkbox y el boton remove
-			$("ul").append('<div><form action="#"><p><input type="checkbox" id="test5"/><label label for="test5" id="comentarios">' + tarea +'</label></p></div>' + '<a class="waves-effect waves-light btn btn-mio remover">Remove</a>');
-	
+			$("ul").append('<li><input type="checkbox"/>' + tarea + '<a class="waves-effect waves-light btn btn-mio remover">Remove</a></li>');
+			$("#comentarios").val("");
 		}
 	});	
-		$(document).on('click' , '.remover' , function(){
+		$(document).on("click", ".remover", function(){
 		$(this).parent().remove();
  	});
+
+	//if( $("#test5").is(':checked') ) { evalua si el checkbox esta presionado, no me funciono
+    //alert('Seleccionado');}
+
 })
